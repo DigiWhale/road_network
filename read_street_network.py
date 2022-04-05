f3e9e01868dddeb8946b69ac1ee377a542b3f48f
digiwhale <digiwhale.humphrey@gmail.com>
2022-04-05 14:56:53 -0400

    1

--- read_street_network.py
+++ read_street_network.py
@@ -1,15 +1,20 @@
+import traceback
 from pyrosm import OSM
 from pyrosm import get_data
+import traceback
 
 # Pyrosm comes with a couple of test datasets 
 # that can be used straight away without
 # downloading anything
-fp = get_data("maryland")
+try:
+  fp = get_data("maryland", update=False, directory='/home/pi/map_files')
 
-# Initialize the OSM parser object
-osm = OSM(fp)
+  # Initialize the OSM parser object
+  osm = OSM(fp)
 
-# Read all drivable roads
-# =======================
-drive_net = osm.get_network(network_type="driving")
-print(drive_net)
+  # Read all drivable roads
+  # =======================
+  drive_net = osm.get_network(network_type="driving")
+  print(drive_net)
+except:
+  print(traceback.format_exc())
